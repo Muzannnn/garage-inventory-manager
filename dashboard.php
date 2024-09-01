77a747ab88f55aeca59e8c698637ca434341663f
Muzan <46395979+Muzannnn@users.noreply.github.com>
2024-09-01 21:31:37 +0200

    add favicon

--- dashboard.php
+++ dashboard.php
@@ -10,7 +10,7 @@ if(!Account::isAuthentified()){
 
 <head>
   <meta charset="utf-8" />
-  <link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="assets/img/favicon.png">
   <link rel="icon" type="image/png" href="assets/img/favicon.png">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
   <title>
@@ -30,12 +30,12 @@ if(!Account::isAuthentified()){
       <div class="logo">
         <a href="https://www.creative-tim.com" class="simple-text logo-mini">
           <div class="logo-image-small">
-            <img src="assets/img/logo-small.png">
+            <img src="assets/img/favicon.png">
           </div>
           <!-- <p>CT</p> -->
         </a>
         <a href="https://www.creative-tim.com" class="simple-text logo-normal">
-          Creative Tim
+          GIM
           <!-- <div class="logo-image-big">
             <img src="../assets/img/logo-big.png">
           </div> -->
@@ -46,7 +46,7 @@ if(!Account::isAuthentified()){
           <li class="active ">
             <a href="./dashboard.html">
               <i class="nc-icon nc-bank"></i>
-              <p>Dashboard</p>
+              <p>Tableau de bord</p>
             </a>
           </li>
           <li>
